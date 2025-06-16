5b91f201fa77139dfc6fac441a0e9751862be13a
xotnet <150250186+xotnet@users.noreply.github.com>
2025-06-16 12:42:12 +0000

    Update net.c

--- net.c
+++ net.c
@@ -91,28 +91,28 @@ int32_t close_net(int32_t conn) {
 }
 
 uint8_t socks5_connect(int32_t sock, const char *ip, uint16_t port) {
-    char buf[64];
-    buf[0] = 0x05;
-    buf[1] = 0x01;
-    buf[2] = 0x00;
-    send_net(sock, buf, 3);
-    recv_net(sock, buf, 2);
-    if (buf[1] != 0x00) {
-        return 1;
-    }
-    buf[0] = 0x05;
-    buf[1] = 0x01;
-    buf[2] = 0x00;
-    buf[3] = 0x01;
-    inet_pton(AF_INET, ip, &buf[4]);
-    buf[8] = (port >> 8) & 0xFF;
-    buf[9] = port & 0xFF;
-    send_net(sock, buf, 10);
-    recv_net(sock, buf, 10);
-    if (buf[1] != 0x00) {
-        return (unsigned short int)buf[1];
-    }
-    return 0;
+	char buf[64];
+	buf[0] = 0x05;
+	buf[1] = 0x01;
+	buf[2] = 0x00;
+	send_net(sock, buf, 3);
+	recv_net(sock, buf, 2);
+	if (buf[1] != 0x00) {
+		return 1;
+	}
+	buf[0] = 0x05;
+	buf[1] = 0x01;
+	buf[2] = 0x00;
+	buf[3] = 0x01;
+	inet_pton(AF_INET, ip, &buf[4]);
+	buf[8] = (port >> 8) & 0xFF;
+	buf[9] = port & 0xFF;
+	send_net(sock, buf, 10);
+	recv_net(sock, buf, 10);
+	if (buf[1] != 0x00) {
+		return (unsigned short int)buf[1];
+	}
+	return 0;
 }
 char dnsIP[16] = "1.1.1.1";
 enum dnsType {dnsANY, dnsA = 1, dnsNS, dnsMD, dnsMF, dnsCNAME, dnsSOA, dnsMB, dnsMG, dnsMR, dnsMX, dnsTXT=16, dnsRP, dnsAFSDB, dnsAAAA=28, dnsLOC, dnsSRV=33, dnsHTTPS=65, dnsSPF=99, dnsCAA=257};
@@ -146,93 +146,116 @@ int8_t resolve_net(char* domain, char* output, uint16_t nsType) {
 	buf[typePos+1] = (unsigned char)(nsType & 0xFF);
 	//buf[typePos+2] = 0x00;
 	buf[typePos+3] = 0x01;
-    send_net(conn, buf, typePos+4);
-    memset(buf, 0, 512);
-    if (recv_net(conn, buf, 512) < domainLen) { // net error
-    	strcpy(output, "Net error");
-    	return -1;
+	send_net(conn, buf, typePos+4);
+	memset(buf, 0, 512);
+	if (recv_net(conn, buf, 512) < domainLen) { // net error
+		strcpy(output, "Net error");
+		return -1;
 	}
-    uint16_t answerCount = (buf[6] << 8) | (buf[7]);
-    if (answerCount == 0) { // no results
-    	strcpy(output, "No results");
-    	return -2;
+	uint16_t answerCount = (buf[6] << 8) | (buf[7]);
+	if (answerCount == 0) { // no results
+		strcpy(output, "No results");
+		return -2;
 	}
 	uint16_t answStart = 12 + domainLen+2 + 4; // header(12) + QName + QTYPEQCLASS(4)
-    for (uint8_t i = 0; i<answerCount; i++) {
-    	// answStart + 0  | 2b NAME pointer
-    	// answStart + 2  | 2b Dns type
-    	// answStart + 4  | 2b Class
-    	// answStart + 6  | 4b TTL
-    	// answStart + 10 | 2b RDATA len
-    	// answStart + 12 | xb RDATA
-    	uint16_t rdatalen = (buf[answStart+10] << 8) | (buf[answStart+11]) - 1;
-    	uint16_t outputLen = strlen(output);
-    	uint8_t addSpace = 0;
-    	if (outputLen != 0) {addSpace = 1;}
-    	if (nsType == dnsA) { // A
-    		char ipbytes[4];
-    		strcpy(ipbytes, buf+answStart+12);
-    		inet_ntop(AF_INET, ipbytes, output+outputLen+addSpace, 15);
-    	} else if (nsType == dnsCAA) { // CAA
-
-    	} else if (nsType == dnsSRV) { // SRV
-    		itos((buf[answStart+12] << 8) | (buf[answStart+13] & 0xFF), output); // priority
-    		uint16_t outputInd = strlen(output);
-    		output[outputInd] = ' ';
-    		itos((buf[answStart+14] << 8) | (buf[answStart+15] & 0xFF), output+outputInd+1); // weight
-    		outputInd = strlen(output);
-    		output[outputInd] = ' ';
-    		itos((buf[answStart+16] << 8) | (buf[answStart+17] & 0xFF), output+outputInd+1); // port
-    		outputInd = strlen(output);
-    		output[outputInd] = ' ';
-    		// xxxx2eu3com3org0
-    		outputInd++;
-    		uint8_t lblLen = buf[answStart+18];
-		    int i=0;
-		    while(1) {
-		    	for(int u = 0;u<lblLen;u++) {
-		    		output[outputInd+i] = buf[answStart+19+i];
-		    		i++;
-		    	}
-		    	lblLen = buf[answStart+19+i];
-		    	if (lblLen == 0) {break;}
-		    	output[outputInd+i] = '.';
-		    	i++;
-		    }
-    	} else { // TXT, CNAME...
-    		uint16_t e = 0;
-	    	for (; e<rdatalen; e++) {
+	for (uint8_t i = 0; i<answerCount; i++) {
+		// answStart + 0  | 2b NAME pointer
+		// answStart + 2  | 2b Dns type
+		// answStart + 4  | 2b Class
+		// answStart + 6  | 4b TTL
+		// answStart + 10 | 2b RDATA len
+		// answStart + 12 | xb RDATA
+		uint16_t rdatalen = (buf[answStart+10] << 8) | (buf[answStart+11]) - 1;
+		uint16_t outputLen = strlen(output);
+		uint8_t addSpace = 0;
+		if (outputLen != 0) {addSpace = 1;}
+		if (nsType == dnsA) { // A
+			char ipbytes[4];
+			strcpy(ipbytes, buf+answStart+12);
+			inet_ntop(AF_INET, ipbytes, output+outputLen+addSpace, 15);
+		} else if (nsType == dnsCAA) { // CAA
+			itos(buf[answStart+12], output); // flags
+			uint32_t outputInd = strlen(output);
+			output[outputInd] = ' ';
+			outputInd++;
+			uint8_t tagSize = buf[answStart+13];
+			for (uint8_t i=0;i<tagSize;i++) { // issue
+				output[outputInd] = buf[answStart+14+i];
+				outputInd++;
+			}
+			output[outputInd] = ' ';
+			outputInd++;
+			answStart += 14+tagSize;
+			uint8_t lblLen = buf[answStart];
+			int32_t i=0;
+			while(1) {
+				for(uint8_t u = 0;u<lblLen;u++) {
+					output[outputInd+i] = buf[answStart+i];
+					i++;
+				}
+				lblLen = buf[answStart+i];
+				if (lblLen == 0) {break;}
+				output[outputInd+i] = '.';
+				i++;
+			}
+		} else if (nsType == dnsSRV) { // SRV
+			itos((buf[answStart+12] << 8) | (buf[answStart+13] & 0xFF), output); // priority
+			uint16_t outputInd = strlen(output);
+			output[outputInd] = ' ';
+			itos((buf[answStart+14] << 8) | (buf[answStart+15] & 0xFF), output+outputInd+1); // weight
+			outputInd = strlen(output);
+			output[outputInd] = ' ';
+			itos((buf[answStart+16] << 8) | (buf[answStart+17] & 0xFF), output+outputInd+1); // port
+			outputInd = strlen(output);
+			output[outputInd] = ' ';
+			// xxxx2eu3com3org0
+			outputInd++;
+			uint8_t lblLen = buf[answStart+18];
+			int32_t i=0;
+			while(1) {
+				for(uint8_t u = 0;u<lblLen;u++) {
+					output[outputInd+i] = buf[answStart+19+i];
+					i++;
+				}
+				lblLen = buf[answStart+19+i];
+				if (lblLen == 0) {break;}
+				output[outputInd+i] = '.';
+				i++;
+			}
+		} else { // TXT, CNAME...
+			uint16_t e = 0;
+			for (; e<rdatalen; e++) {
 				char rdata[rdatalen];
 				rdata[e] = buf[answStart+13+e];
 				strcpy(output+outputLen+addSpace, rdata);
-	    	}
-	    	output[e] = 0;
-    	}
-    	answStart = answStart+rdatalen+12;
-    	i++;
-    }
-    close_net(conn);
-    return 0;
+			}
+			output[e] = 0;
+		}
+		answStart = answStart+rdatalen+12;
+		i++;
+	}
+	close_net(conn);
+	return 0;
 }
 
 void itos(int32_t N, char* str) {
-    int32_t isNegative = 0;
-    if (N < 0) {
-        isNegative = 1;
-        N = -N;
-    }
-    int32_t index = 0;
-    do {
-        str[index++] = (N % 10) + '0';
-        N /= 10;
-    } while (N > 0);
-    if (isNegative) {
-        str[index++] = '-';
-    }
-    str[index] = '\0';
-    for (int32_t i = 0; i < index / 2; i++) {
-        char temp = str[i];
-        str[i] = str[index - i - 1];
-        str[index - i - 1] = temp;
-    }
+	int32_t isNegative = 0;
+	if (N < 0) {
+		isNegative = 1;
+		N = -N;
+	}
+	int32_t index = 0;
+	do {
+		str[index++] = (N % 10) + '0';
+		N /= 10;
+	} while (N > 0);
+	if (isNegative) {
+		str[index++] = '-';
+	}
+	str[index] = '\0';
+	for (int32_t i = 0; i < index / 2; i++) {
+		char temp = str[i];
+		str[i] = str[index - i - 1];
+		str[index - i - 1] = temp;
+	}
 }
